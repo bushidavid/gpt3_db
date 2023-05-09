7ee93498729089176674ecc54cc80c243ce3e921
davidbushi <88148058+davidbushi@users.noreply.github.com>
2023-05-09 16:36:15 +0200

    second commit

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-import './possibility.css'
+import './possibility.css';
+import possibilityImg from "../../assets/possibility.png";
 
 const Possibility = () => {
     return ( 
-        <h1> Possibility</h1>
+        <div className="gpt3__possibility section__padding" id="possibility">
+            <div className="gpt3__possibility-image">
+                <img src={possibilityImg}></img>
+            </div>
+            <div className="gpt3__possibility-content">
+                <h4>Request early access to get started</h4>
+                <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                <h4>Request early access to get started</h4>
+            </div>
+        </div>
      );
 }
  
